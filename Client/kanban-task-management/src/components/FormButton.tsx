6082b3ecb1016ef519753e86efc988d24ec625b0
unknown <kaludivine545@gmail.com>
2023-11-29 08:11:28 +0100

    added some changes to my input and button component

--- Client/kanban-task-management/src/components/FormButton.tsx
+++ Client/kanban-task-management/src/components/FormButton.tsx
@@ -4,7 +4,7 @@ const ButtonComponent: FC<ButtonProps> = ({ title, type, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className='md:w-[380px] sm:w-[350px] text-[#fff]  w-[280px]  p-[8px] bg-[#635FC7] font-semibold capitalize rounded-[24px] hover:bg-[#A8A4FF]'
+      className='md:w-[380px] sm:w-[350px] text-[#fff]  w-[300px]  p-[8px] bg-[#635FC7] font-semibold capitalize rounded-[24px] hover:bg-[#A8A4FF]'
       type={type}
     >
       {title}
